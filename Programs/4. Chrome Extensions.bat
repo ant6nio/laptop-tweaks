db2bf9a02a65dd07987ee669ebbd2ae541285c37
ant6nio <83166007+ant6nio@users.noreply.github.com>
2021-06-09 22:57:57 +0100

    Add files via upload

---
+++ Programs/4. Chrome Extensions.bat
@@ -0,0 +1,3 @@
+@echo off
+start https://chrome.google.com/webstore/detail/dark-reader/eimadpbcbfnmbkopoojfekhnkhdbieeh
+start https://chrome.google.com/webstore/detail/https-everywhere/gcbommkclmclpchllfjekcdonpmejbdp
