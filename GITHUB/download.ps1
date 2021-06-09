c7fa7febe6b3531ace11d385ef071f4a86aa8a20
ant6nio <83166007+ant6nio@users.noreply.github.com>
2021-06-09 23:44:41 +0100

    Update download.ps1

--- GITHUB/download.ps1
+++ GITHUB/download.ps1
@@ -1,2 +1,2 @@
 cd $env:userprofile\Desktop
-powershell git clone "https://github.com/ant6nio/laptop-tweaks/main.zip"
+Invoke-WebRequest "https://github.com/ant6nio/laptop-tweaks/archive/refs/tags/v1.zip" -OutFile "%userprofile%\Desktop\laptop-tweaks-1.zip"
