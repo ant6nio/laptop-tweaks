8995b3eb28f6b491f58e4cbd10ebd2d8095a208f
ant6nio <83166007+ant6nio@users.noreply.github.com>
2021-06-09 23:38:41 +0100

    Update download.ps1

--- GITHUB/download.ps1
+++ GITHUB/download.ps1
@@ -1,3 +1,2 @@
 cd $env:userprofile\Desktop
-powershell git clone https://github.com/ant6nio/laptop-tweaks/main.zip
-Start-Process $env:userprofile\Desktop\couleurstoolbox
+powershell git clone "https://github.com/ant6nio/laptop-tweaks/main.zip"
